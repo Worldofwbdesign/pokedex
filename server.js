bc76af25fe04adb1b45ecd8f0085ec94d0202227
Worldofwbdesign <worldofwbdesign@gmail.com>
2017-09-10 22:05:07 +0300

    fix for heroku deployment

--- server.js
+++ server.js
@@ -4,16 +4,8 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function(req, res, next) {
-	if (req.headers['x-forwarded-proto'] === 'https') {
-		res.redirect('http://' + req.hostname + req.url);
-	} else {
-		next();
-	}
-})
-
 app.use(express.static('dist'));
 
 app.listen(PORT, function(){
-	console.log('The server is running in 3000 port right now!' + PORT);
+	console.log('The server is running in' + PORT);
 })
